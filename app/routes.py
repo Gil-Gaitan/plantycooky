61827964f1ec3bc2c31404b344a4288022b24bb3
Gil Gaitan IV <gaitan@fastmail.com>
2024-10-14 12:29:22 -0500

    Registration Feature; Logout Feature

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from app.forms import LoginForm
 from flask_login import current_user, login_user
 import sqlalchemy as sa
 from app import db
+from app.forms import RegistrationForm
 from app.models import User
 from flask_login import logout_user
 from flask_login import login_required
@@ -45,4 +46,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 # flask run --port 5001
