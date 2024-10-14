7f2b1c403ed78851150708e1dbbf6399551ece7f
Gil Gaitan IV <gaitan@fastmail.com>
2024-10-14 10:00:07 -0500

    Password Hashing

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -15,6 +16,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
